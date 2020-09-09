a9b5449cf51943a0ec4d86b658d27246cc245bc7
ajtech <ajtech129@gmail.com>
2020-09-09 21:08:33 +0530

    carousel and vertical auto scroll latest news added

---
+++ js/custom-script.js
@@ -0,0 +1,17 @@
+$(function(){
+    var tickerLength = $('.v-scroll ul li').length;
+    var tickerHeight = $('.v-scroll ul li').outerHeight();
+    $('.v-scroll ul li:last-child').prependTo('.v-scroll ul');
+    $('.v-scroll ul').css('marginTop',-tickerHeight);
+    function moveTop(){
+      $('.v-scroll ul').animate({
+        top : -tickerHeight
+      },600, function(){
+       $('.v-scroll ul li:first-child').appendTo('.v-scroll ul');
+        $('.v-scroll ul').css('top','');
+      });
+     }
+    setInterval( function(){
+      moveTop();
+    }, 3000);
+    });
